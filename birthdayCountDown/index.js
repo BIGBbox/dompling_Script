891a04ae62088e69a1b2eefb782752759001de34
dompling <374779789@qq.com>
2020-08-25 22:55:16 +0800

    更新图片不显示

--- birthdayCountDown/index.js
+++ birthdayCountDown/index.js
@@ -24,8 +24,8 @@ cron "10 0 0 * * *" script-path=https://raw.githubusercontent.com/dompling/Scrip
 
 
 var mediaImg = ""; // 头像图片默认显示第一张图片，随机 bing 应 api 背景
-var username = ""; // 姓名
-var birthday = ""; // 生日日期
+var username = "小可爱"; // 姓名
+var birthday = "1995-6-28"; // 生日日期
 var physiologicalDefault = ""; // 最近一次来周期时间
 var physiologicalCycle = ""; // 下一次周期
 var nongli = false; // 是否农历生日
@@ -34,8 +34,7 @@ const $ = API("birthday", true);
 
 var birthday_mediaImg = $.read("mediaImg");
 if (!mediaImg) mediaImg = birthday_mediaImg;
-$.log("缓存：" + JSON.stringify($.cache));
-$.log("媒体图片：" + mediaImg);
+
 
 var birthday_username = $.read("username");
 if (birthday_username) username = birthday_username;
@@ -60,6 +59,10 @@ const _birthdayConfig = {
   nongli, // 农历生日
   isLeapMonth: false, //如果是农历闰月第四个参数赋值true即可
 };
+
+$.log("基础数据：" +_birthdayConfig);
+$.log("媒体图片：" + mediaImg);
+
 var dataSource = [_birthdayConfig];
 var verify = true;
 for (var i = 0; i < dataSource.length; i++) {
