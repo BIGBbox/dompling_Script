1ce682ea2c60cd2144c4578b6ca2a8e1637e9525
dompling <374779789@qq.com>
2020-08-28 09:53:35 +0800

    猫眼widget

---
+++ maoyan.scriptable/maoyan.js
@@ -0,0 +1,61 @@
+const $ = importModule("Env");
+const maoyanLink = "https://m.maoyan.com/ajax/movieOnInfoList";
+
+const defaultColor = ["#ffa39e","#ff9c6e","#ffc069","#ffd666","#5cdbd3","#69c0ff"];
+try {
+  const response = await getMaoyan();
+  if (response.movieList.length > 0) {
+    createWidget(response);
+  } else {
+    throw new Error("获取内容失败");
+  }
+} catch (e) {
+  console.log("异常：" + e);
+}
+
+function createWidget(res) {
+  if (res.status == "ok") {
+    const w = new ListWidget();
+    const bgColor = new LinearGradient();
+    bgColor.colors = [new Color("#1c1c1c"), new Color("#29323c")];
+    bgColor.locations = [0.0, 1.0];
+    w.backgroundGradient = bgColor;
+    w.centerAlignContent();
+
+    const firstLine = w.addText(`[📣]`);
+    firstLine.textSize = 14;
+    firstLine.textColor = Color.white();
+    firstLine.textOpacity = 0.7;
+
+
+
+    w.presentMedium();
+    return w;
+  }
+}
+
+function renderColorText(data) {
+  const textObj = w.addText(`• ${data.text}`);
+  textObj.textSize = 12;
+  textObj.textColor = new Color(data.color);
+}
+
+
+
+//更新代码
+function update() {
+  log("🔔更新脚本开始!");
+  scripts.forEach(async (script) => {
+    await $.getFile(script);
+  });
+  log("🔔更新脚本结束!");
+}
+
+const scripts = [
+  {
+    moduleName: "RSSMonitor",
+    url:
+      "https://raw.githubusercontent.com/evilbutcher/Scriptables/master/RSSMonitor.js",
+  },
+];
+if (goupdate == true) update();
