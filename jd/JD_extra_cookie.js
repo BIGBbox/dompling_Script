ad0e896e07a80c415fa03fdfa7c3d1da9ba8f269
dompling <374779789@qq.com>
2020-10-28 14:33:25 +0800

    京东多账号获取

--- jd/JD_extra_cookie.js
+++ jd/JD_extra_cookie.js
@@ -1,25 +1,54 @@
 /*****
- * 2YA
- * 
- * ===================
- * 特别说明：
- *    1.获取多个京东cookie文件，不和野比大佬的文件冲突。暂不支持野比大佬脚本签到。
- *    2.若是要使用京东多合一签到，请使用修改版地址：https://raw.githubusercontent.com/dompling/Script/master/jd/JD_extra.js
- * ===================
- *
- * ===================
- * 使用方式：打开 https://home.m.jd.com/myJd/newhome.action ，在个人中心自动获取 cookie，若弹出成功则正常使用。
- * ===================
+2YA
+ 
+===================
+特别说明：
+  1.获取多个京东cookie文件，不和野比大佬的文件冲突。暂不支持野比大佬脚本签到。
+  2.若是要使用京东多合一签到，请使用修改版地址：https://raw.githubusercontent.com/dompling/Script/master/jd/JD_extra.js
+===================
+===================
+使用方式：打开 https://home.m.jd.com/myJd/newhome.action ，在个人中心自动获取 cookie，若弹出成功则正常使用。
+===================
+
+===================
+【Surge脚本配置】:
+===================
+[Script]
+获取京东Cookie = type=http-request,pattern=https:\/\/wq\.jd\.com\/client\.action.*functionId=signBean,script-path=https://raw.githubusercontent.com/dompling/Script/master/jd/JD_extra_cookie.js
+
+[MITM]
+hostname = wq.jd.com
+
+===================
+【Loon脚本配置】:
+===================
+
+http-request https:\/\/wq\.jd\.com.*=GetJDUserInfoUniontag=获取京东Cookie, script-path=https://raw.githubusercontent.com/dompling/Script/master/jd/JD_extra_cookie.js
+
+[MITM]
+hostname = wq.jd.com
+
+===================
+【 QX  脚本配置 】 :
+===================
+
+[rewrite_local]
+https:\/\/wq\.jd\.com.*=GetJDUserInfoUnionurl script-request-header https://raw.githubusercontent.com/dompling/Script/master/jd/JD_extra_cookie.js
+
+[mitm]
+hostname = wq.jd.com
  *
  */
 
 $ = new API("JD_Cookies", true);
 const CookieKey = "CookiesJD";
 if ($request) GetCookie();
+
 function getCache() {
-  var cache = $.read(CookieKey);
-  return cache || [];
+  var cache = $.read(CookieKey) || "[]";
+  return JSON.parse(cache);
 }
+
 function GetCookie() {
   try {
     if (
@@ -51,16 +80,15 @@ function GetCookie() {
         var tipPrefix = "";
         if (updateCodkie) {
           updateCookiesData[updateIndex].cookie = CookieValue;
-          $.write(updateCookiesData, CookieKey);
           tipPrefix = "更新京东";
         } else {
           updateCookiesData.push({
             userName: DecodeName,
             cookie: CookieValue,
           });
-          $.write(updateCookiesData, CookieKey);
           tipPrefix = "首次写入京东";
         }
+        $.write(JSON.stringify(updateCookiesData), CookieKey);
         $.notify(
           "用户名: " + DecodeName,
           "",
